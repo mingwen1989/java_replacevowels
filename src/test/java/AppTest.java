bfd83746ebbaa701889434d2bad599c7d1eaa880
Ming Wen <mwen82@gmail.com>
2016-04-22 13:41:41 -0700

    Add Integration Tests

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -16,5 +16,25 @@ public class AppTest extends FluentTest {
   @ClassRule
   public static ServerRule server = new ServerRule();
 
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Guess the Phrase");
+  }
 
+  @Test
+  public void isAValidInput() {
+    goTo("http://localhost:4567");
+    fill("#userInput").with("temporary");
+    submit(".btn");
+    assertThat(pageSource()).contains("t-mp-r-ry");
+  }
+
+  @Test
+  public void isAValidSolution() {
+    goTo("http://localhost:4567/results?userInput=temporary");
+    fill("#solveInput").with("temporary");
+    submit(".btn");
+    assertThat(pageSource()).contains("temporary");
+  }
 }
