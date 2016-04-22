f7d19b42a6e541caea4aaf2f9c12082b1a85e547
Ming Wen <Ming Wen>
2016-04-21 23:00:24 -0700

    Modify hashmap in response to uppercase and lowercase tests

--- src/main/java/ReplaceVowel.java
+++ src/main/java/ReplaceVowel.java
@@ -5,13 +5,18 @@ public class ReplaceVowel {
 
   public static String runReplaceVowel(String userString) {
     HashMap<String, String> vowelLookUp = new HashMap<String, String>();
+    vowelLookUp.put("a", "-");
+    vowelLookUp.put("e", "-");
+    vowelLookUp.put("i", "-");
+    vowelLookUp.put("o", "-");
+    vowelLookUp.put("u", "-");
     vowelLookUp.put("A", "-");
     vowelLookUp.put("E", "-");
     vowelLookUp.put("I", "-");
     vowelLookUp.put("O", "-");
     vowelLookUp.put("U", "-");
 
-    char[] charArray = userString.toUpperCase().toCharArray();
+    char[] charArray = userString.toCharArray();
     String vowelOutput = new String();
 
     for (int i = 0 ; i < charArray.length ; i++) {
@@ -19,7 +24,7 @@ public class ReplaceVowel {
       if ( vowelLookUp.get(letters) != null ) {
         vowelOutput += vowelLookUp.get(letters);
       } else {
-        vowelOutput += letters.toLowerCase();
+        vowelOutput += letters;
       }
   } return vowelOutput;
 }
--- src/test/java/ReplaceVowelTest.java
+++ src/test/java/ReplaceVowelTest.java
@@ -10,4 +10,18 @@ public class ReplaceVowelTest {
     String expected = "h-ll-";
     assertEquals(expected, replaceVowel.runReplaceVowel("hello"));
   }
+
+  @Test
+  public void calculateNumber_vowelOutput_moderate() {
+    ReplaceVowel replaceVowel = new ReplaceVowel();
+    String expected = "H-ll-, - -m h-ppy.";
+    assertEquals(expected, replaceVowel.runReplaceVowel("Hello, I am happy."));
+  }
+
+  @Test
+  public void calculateNumber_vowelOutput_long() {
+    ReplaceVowel replaceVowel = new ReplaceVowel();
+    String expected = "B-l--v- y-- c-n -nd y--'r- h-lfw-y th-r-. Th--d-r- R--s-v-lt";
+    assertEquals(expected, replaceVowel.runReplaceVowel("Believe you can and you're halfway there. Theodore Roosevelt"));
+  }
 }
